6e540c0982c7d0bd3b86dc579a0386228f0c72f9
Gabriel Del VIllar <gdelvillar@gabriels-macbook-pro.local>
2018-11-03 11:56:00 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gabriel Del VIllar on 11/3/18.
+//  Copyright © 2018 gdelvillar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
